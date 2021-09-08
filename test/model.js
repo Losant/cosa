29882c3a05cd48818cfbec9fafa9127274958863
Erin Thompson <erin@losant.com>
2021-09-08 13:49:32 -0400

    revert test ids

--- test/model.js
+++ test/model.js
@@ -232,12 +232,12 @@ describe('Model', () => {
 
   describe('.saveWithId()', function() {
     it('should save with a given ID', async () => {
-      const id = new bson.ObjectID('1234abcd103f8e485c9d2017');
+      const id = new bson.ObjectID('1234abcd103f8e485c9d2019');
       const model = await FullTestModel.create({
         str: 'foo'
       }).saveWithId(id);
 
-      expect(model._id.toString()).to.equal('1234abcd103f8e485c9d2017');
+      expect(model._id.toString()).to.equal('1234abcd103f8e485c9d2019');
     });
 
     it('should error when trying to update an object', async () => {
